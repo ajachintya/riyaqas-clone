c4da10ba48b2c7cbd2a5cc6306b27c2ea705bba9
deepsingh11421 <deepsingh11421@gmail.com>
2020-10-06 13:52:56 +0530

    Stickey Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import StickeyNav from './Component/Sticky-nav/Stickey-nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <StickeyNav />
     </div>
   );
 }
