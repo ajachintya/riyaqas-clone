4bfb00b918838fbc6de77b8ac3d7755e9c916191
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-10-08 23:00:42 +0530

    header icons

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import classes from "./Header.module.css";
 import photo from '../../assets/images/Screenshot (28).png';
 import Neviagtion from './Navigation/Navigation';
-
+import "font-awesome/css/font-awesome.min.css";
 
 
 export default class Header extends Component {
@@ -16,14 +16,32 @@ export default class Header extends Component {
 
         return (
           <div className={headClass}>
-            <img className={classes.img} src={photo} alt="logo"></img>
-            <div className={classes.div}>
-              <Neviagtion name="Home" />
-              <Neviagtion name="Shop" />
-              <Neviagtion name="Blog" />
-              <Neviagtion name="Pages" />
-              <Neviagtion name="Career" />
-              <Neviagtion name="Contact" />
+            <div className={classes.Navlogo}>
+              <img className={classes.img} src={photo} alt="logo"></img>
+              <div className={classes.div}>
+                <Neviagtion name="Home" />
+                <Neviagtion name="Shop" />
+                <Neviagtion name="Blog" />
+                <Neviagtion name="Pages" />
+                <Neviagtion name="Career" />
+                <Neviagtion name="Contact" />
+              </div>
+            </div>
+            <div className={classes.Icon}>
+              <i
+                style={{ "paddingRight": "40px" }}
+                class="fa fa-search"
+                aria-hidden="true"
+              ></i>
+              <i
+                style={{ "paddingRight": "40px" }}
+                class="fa fa-cart-plus"
+                aria-hidden="true"
+              ></i>
+              <i
+                class="fa fa-heart-o"
+                aria-hidden="true"
+              ></i>
             </div>
           </div>
         );
