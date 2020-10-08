9e8926f4b8e8db25888f808cbd8dfb5c912dbbd6
deepsingh11421 <deepsingh11421@gmail.com>
2020-10-08 21:10:53 +0530

    Updates

---
+++ precache-manifest.15a6b63b783a109cebe6e5e5b1fb7ad7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62abf428e4cb9cdf4bfd0577f40d1c87",
+    "url": "//ajachintya.github.io/riyaqas-clone/index.html"
+  },
+  {
+    "revision": "041ac06ec3e25c160d77",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/css/2.78edd654.chunk.css"
+  },
+  {
+    "revision": "cef9d91d36fa74b37c2b",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/css/main.807fecc6.chunk.css"
+  },
+  {
+    "revision": "041ac06ec3e25c160d77",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/js/2.9b666bff.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/js/2.9b666bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef9d91d36fa74b37c2b",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/js/main.20d933bd.chunk.js"
+  },
+  {
+    "revision": "a311f2320b60e819ae84",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/js/runtime-main.5ce4e4ce.js"
+  },
+  {
+    "revision": "095bda5041c7f5314b105e02a3031329",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/checklist.095bda50.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e45005580e28507efb35527682fcd786",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/user.e4500558.png"
+  },
+  {
+    "revision": "62837935c39a00adc077206bc5b9262b",
+    "url": "//ajachintya.github.io/riyaqas-clone/static/media/user3.62837935.png"
+  }
+]);
