93fe9662b07c9a0f73950587076cb9333579b2b5
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-10-09 13:21:29 +0530

    header ham

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -6,44 +6,43 @@ import "font-awesome/css/font-awesome.min.css";
 
 
 export default class Header extends Component {
-    render() {
-        var headClass = [classes.Header];
-        if(this.props.stickey === true){
-          headClass.push(classes.stickey);
-        }
+  hamToCrossHandler = () => {
+    
+  }
 
-        headClass = headClass.join(' ');
-
-        return (
-          <div className={headClass}>
-            <div className={classes.Navlogo}>
-              <img className={classes.img} src={photo} alt="logo"></img>
-              <div className={classes.div}>
-                <Neviagtion name="Home" />
-                <Neviagtion name="Shop" />
-                <Neviagtion name="Blog" />
-                <Neviagtion name="Pages" />
-                <Neviagtion name="Career" />
-                <Neviagtion name="Contact" />
-              </div>
-            </div>
-            <div className={classes.Icon}>
-              <i
-                style={{ "paddingRight": "40px" }}
-                class="fa fa-search"
-                aria-hidden="true"
-              ></i>
-              <i
-                style={{ "paddingRight": "40px" }}
-                class="fa fa-cart-plus"
-                aria-hidden="true"
-              ></i>
-              <i
-                class="fa fa-heart-o"
-                aria-hidden="true"
-              ></i>
-            </div>
-          </div>
-        );
+  render() {
+    var headClass = [classes.Header];
+    if (this.props.stickey === true) {
+      headClass.push(classes.stickey);
     }
+
+    headClass = headClass.join(" ");
+
+    return (
+      <div className={headClass}>
+        <div className={classes.Navlogo}>
+          <img className={classes.img} src={photo} alt="logo"></img>
+          <div className={classes.div}>
+            <Neviagtion name="Home" />
+            <Neviagtion name="Shop" />
+            <Neviagtion name="Blog" />
+            <Neviagtion name="Pages" />
+            <Neviagtion name="Career" />
+            <Neviagtion name="Contact" />
+          </div>
+        </div>
+        <div className={classes.Icon}>
+          <i class="fa fa-search" aria-hidden="true"></i>
+          <i class="fa fa-cart-plus" aria-hidden="true"></i>
+          <i class="fa fa-heart-o" aria-hidden="true"></i>
+          <i
+            id={classes.Dropdown}
+            onClick={this.hamToCrossHandler}
+            class="fa fa-bars"
+            aria-hidden="true"
+          ></i>
+        </div>
+      </div>
+    );
+  }
 }
