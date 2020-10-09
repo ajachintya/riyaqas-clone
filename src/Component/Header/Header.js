03a5c7fbc4d88eed7a368749544259c6694fd477
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-10-09 20:36:37 +0530

    new nav

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import classes from "./Header.module.css";
-import photo from '../../assets/images/Screenshot (28).png';
+import photo from '../../assets/images/Screenshot (31).png';
 import Neviagtion from './Navigation/Navigation';
 import "font-awesome/css/font-awesome.min.css";
+import { Button } from '@material-ui/core';
 
 
 export default class Header extends Component {
   hamToCrossHandler = () => {
-    
-    
+
   }
 
   render() {
@@ -22,26 +22,16 @@ export default class Header extends Component {
     return (
       <div className={headClass}>
         <div className={classes.Navlogo}>
-          <img className={classes.img} src={photo} alt="logo"></img>
-          <div className={classes.div}>
-            <Neviagtion name="Home" />
-            <Neviagtion name="Shop" />
-            <Neviagtion name="Blog" />
-            <Neviagtion name="Pages" />
-            <Neviagtion name="Career" />
-            <Neviagtion name="Contact" />
+          <div>
+            <img className={classes.img} src={photo} alt="logo"></img>
           </div>
-        </div>
-        <div className={classes.Icon}>
-          <i class="fa fa-search" aria-hidden="true"></i>
-          <i class="fa fa-cart-plus" aria-hidden="true"></i>
-          <i class="fa fa-heart-o" aria-hidden="true"></i>
-          <i
-            id={classes.Dropdown}
-            onClick={this.hamToCrossHandler}
-            class="fa fa-bars"
-            aria-hidden="true"
-          ></i>
+          <div className={classes.div}>
+            <Neviagtion name="Product" />
+            <Neviagtion name="SignIn" />
+            <Button id={classes.Button} variant="contained" color="secondary">
+              Secondary
+            </Button>
+          </div>
         </div>
       </div>
     );
