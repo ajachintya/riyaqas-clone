b0467b75ae5f8a2b60e29ecfc610187a00d1633d
deepsingh11421 <deepsingh11421@gmail.com>
2020-10-13 14:05:02 +0530

    Header dropdown

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -7,8 +7,13 @@ import { Button } from '@material-ui/core';
 
 
 export default class Header extends Component {
-  hamToCrossHandler = () => {
+  state = {
+    dropdown: false
+  }
 
+  hamToCrossHandler = () => {
+    var val = this.state.dropdown;
+    this.setState({dropdown: !val});
   }
 
   render() {
@@ -18,6 +23,15 @@ export default class Header extends Component {
     }
 
     headClass = headClass.join(" ");
+    var droplistClass = [classes.droplist];
+    var dropicon = <i id={classes.ham} class="fa fa-bars" onClick={this.hamToCrossHandler}></i>
+    if(this.state.dropdown){
+      droplistClass.push(classes.show);
+      dropicon = <i id={classes.ham} class="fa fa-times" onClick={this.hamToCrossHandler}></i>
+    }
+
+    droplistClass = droplistClass.join(" ");
+    
 
     return (
       <div className={headClass}>
@@ -25,14 +39,25 @@ export default class Header extends Component {
           <div>
             <img className={classes.img} src={photo} alt="logo"></img>
           </div>
-          <i id={classes.ham} class="fa fa-bars"></i>
+          {dropicon}
+          <div className={droplistClass}>
+            <Neviagtion name="Product" droplist="true"/>
+            <Neviagtion name="Pricing" droplist="true"/>
+            <Neviagtion name="OurStory" droplist="true"/>
+            <Neviagtion name="SignIn" droplist="true"/>
+            <div className={classes.btnContain}>
+                <Button id={classes.Button} variant="contained">
+                  Request A Demo
+                </Button>
+            </div>
+          </div>
           <div className={classes.div}>
             <Neviagtion name="Product" />
             <Neviagtion name="Pricing" />
             <Neviagtion name="OurStory" />
             <Neviagtion name="SignIn" />
             <Button id={classes.Button} variant="contained">
-              Requst A Demo
+              Request A Demo
             </Button>
           </div>
         </div>
--- src/Component/Header/Navigation/Navigation.js
+++ src/Component/Header/Navigation/Navigation.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import classes from './Navigation.module.css';
 export default class Navigation extends Component {
     render(props) {
-        return (
-          <div className={classes.Nevigation}>
+        var display = <div className={classes.Nevigation}>
+                        {this.props.name}
+                      </div>;
+        if(this.props.droplist){
+          display = <div className={classes.dropitem}>
             {this.props.name}
+          </div>
+        }
+
+        return (
+          <div>
+            {display}
           </div>
         );
     }
