e2ce62cbcb52c32820edb1c25123a2a7761aba8a
deepsingh11421 <deepsingh11421@gmail.com>
2020-10-16 22:12:04 +0530

    Responsive

--- src/Component/RequestDemo/RequestDemo.js
+++ src/Component/RequestDemo/RequestDemo.js
@@ -1,27 +1,23 @@
 import React from 'react'
-import photo1 from '../../assets/images/pexels-anna-shvets-4226263 (2).jpg';
-import photo2 from "../../assets/images/gabriel-benois-qnWPjzewewA-unsplash (1).jpg";
+import photo from '../../assets/images/pixels.jpg';
 import classes from './RequestDemo.module.css';
 import { Button } from '@material-ui/core';
 
 export default function RequestDemo() {
     return (
       <div className={classes.Request}>
-        <div style={{textAlign:"center"}}>
-          <h1>Request A Demo</h1>
-        </div>
         <div className={classes.second}>
-          <div className={classes.img}>
-            <img src={photo1} alt="logo"></img>
+          <div className={classes.img} style={{backgroundImage: `url(${photo})`,backgroundSize: 'cover'}}>
+            {/* <img src={photo1} alt="logo"></img> */}
           </div>
           <div className={classes.div2}>
-            <h1 style={{marginTop:"0px"}}>Why settle for less when you can have so much more!</h1>
-            <Button style={{marginTop:"25px"}} variant="contained" color="primary" disableElevation>
-              Request A Demo
-            </Button>
-            <br />
-            <div className={classes.img2}>
-              <img src={photo2} alt="abc"></img>
+            <div>
+              <h1 style={{marginTop:"0px"}}>Why settle for less when you can have so much more!</h1>
+            </div>
+            <div>
+              <Button style={{marginTop:"25px",width: "250px",padding: "10px 20px",fontSize: '1.2rem'}} variant="contained" color="primary" disableElevation>
+                Request A Demo
+              </Button>
             </div>
           </div>
         </div>
