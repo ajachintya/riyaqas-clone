fa0994bbafa479b2698cd0b325cc5dd2abf43cb7
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-10-16 18:49:16 +0530

    req component

--- src/Component/RequestDemo/RequestDemo.js
+++ src/Component/RequestDemo/RequestDemo.js
@@ -1,25 +1,22 @@
-import React,{Component} from 'react'
+import React from 'react'
 import classes from './RequestDemo.module.css';
 import { Button } from '@material-ui/core';
 import photo1 from '../../assets/images/Screenshot (3fsd6).jpg'
 import photo2 from "../../assets/images/gabriel-benois-qnWPjzewewA-unsplash.jpg";
 
-export default class RequestDemo extends Component {
-    render() {
-        return (
-          <div className={classes.Request}>
-            <div>
-              <img scr={photo1} alt="conference"></img>
-            </div>
-            <div>
-              <h1>Why settle for less when you can have so much more!</h1>
-              <Button variant="contained" color="primary" disableElevation>
-                Request A Demo
-              </Button>{" "}
-              <br />
-              <img scr={photo2} alt="pto"></img>
-            </div>
-          </div>
-        );
-    }
+export default function RequestDemo() {
+    return (
+      <div className={classes.Request}>
+        <div>
+          <img scr={photo1} alt="conference"></img>
+        </div>
+        <div>
+          <h1>Why settle for less when you can have so much more!</h1>
+          <Button variant="contained" color="primary" disableElevation>
+            Request A Demo
+          </Button> <br/>
+          <img scr={photo2} alt="pto"></img>
+        </div>
+      </div>
+    );
 }
